6fe776883774df1f4208a8b6244f137d0bd98d96
Ujs-zhx <780653871@qq.com>
2019-11-26 16:40:12 +0800

    新建文件

---
+++ src/com/atguigu/egit/test/EgitTest.java
@@ -0,0 +1,13 @@
+package com.atguigu.egit.test;
+
+public class EgitTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("aaa");
+		System.out.println("bbb");
+		System.out.println("ccc");
+	}
+
+}
+
